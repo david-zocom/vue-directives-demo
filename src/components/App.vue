9f63d46e706741a5ef3d62950a7f76ceda7f82d2
David Andersson <David Andersson>
2018-04-04 11:44:29 +0200

    First component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,26 +1,22 @@
 <template>
   <div id="app">
-    <div class="banner">
-      <img
-        src="https://vuejs.org/images/logo.png"
-        width="100"
-        alt="vue"
-        class="logo"
-      />
-      <h1>Welcome to Vue.js</h1>
-    </div>
-    <div class="bottom">
-      To get started, edit <code>./src/components/App.vue</code> and save to reload.<br/>
-      <span class="fade">
-        Checkout <code>./README.md</code> for more usages.
-      </span>
-    </div>
+    Tomte! {{ color }}
   </div>
 </template>
 
 <script>
   export default {
-    name: 'app'
+    name: 'app',
+	data: function() {
+		return {
+			color: 'magic',
+			another: true,
+			user: {
+				name: '',
+				email: ''
+			}
+		};
+	}  // data
   }
 </script>
 
